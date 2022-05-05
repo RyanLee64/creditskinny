9055b4e9fd9848193bcdf7fd5034314128441a32
Ryan Lee <dbl2127@columbia.edu>
2022-05-05 17:40:40 -0400

    hitting test API endpoint working with cognito authorization

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,14 +1,46 @@
 import React from 'react';
-import {Amplify} from 'aws-amplify';
+import {Amplify, Auth, API} from 'aws-amplify';
 import { AmplifyAuthenticator, AmplifySignOut, AmplifySignUp } from '@aws-amplify/ui-react';
 import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components';
 import awsconfig from '../aws-exports';
+Amplify.configure({  API: {
+    endpoints: [
+      {
+        name: "AI Customer Service API",
+        endpoint:
+          "https://wa7amainke.execute-api.us-east-1.amazonaws.com/dev",
+      },
+    ],
+  }});
 
-Amplify.configure(awsconfig);
 
 const AuthStateApp = () => {
   const [authState, setAuthState] = React.useState();
   const [user, setUser] = React.useState();
+  const apiName = "AI Customer Service API";
+  const path = "/chatbot";
+  
+
+  const onClick = async () => 
+    {   
+        let user = await Auth.currentAuthenticatedUser();
+        let token = user.signInUserSession.idToken.jwtToken;
+        console.log(token)
+        let request = {
+            headers: {"Authorization": token},
+            body: 
+                JSON.stringify({messages: [{
+                  type: 'unstructured',
+                  unstructured: {
+                    text: "Hi im paul"
+                  }
+                }]})
+            
+        };
+        API.post(apiName,path ,request).then((res) => console.log(res));
+        
+
+    };
 
   React.useEffect(() => {
     return onAuthUIStateChange((nextAuthState, authData) => {
@@ -18,7 +50,14 @@ const AuthStateApp = () => {
   }, []);
 
   return authState === AuthState.SignedIn && user ? (
+    
     <div className="App">
+
+        <button
+        onClick={() => onClick()}
+        className="btn btn-primary">
+        Make a Call
+        </button>
       <div>Hello, {user.username}{console.log(user)}</div>
       <AmplifySignOut />
     </div>
